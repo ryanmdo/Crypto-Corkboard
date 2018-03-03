8a861dfc68b556af2368c632bc462271edb5583e
rdo <ryan@libertylearner.com>
2018-03-03 11:28:45 -0800

    DOM not rendering

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
+const path = require('path')
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
+
+
+app.use(express.static(__dirname + '/client/public'));
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, "/client/public/index.html"));
+})
 
 
 
